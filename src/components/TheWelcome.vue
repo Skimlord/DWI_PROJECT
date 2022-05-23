ee0aff4c49a9c2dafa6bb2e7a213fcb4a4372343
hiram.rubio <floreshiram03@gmail.com>
2022-05-23 18:25:48 -0500

    initial commit

---
+++ src/components/TheWelcome.vue
@@ -0,0 +1,11 @@
+<script></script>
+<template>
+  <main>
+    <div class="text-3xl font-bold flex justify-evenly items-center p-10">
+      <div>Hello World!</div>
+      <div>Hello World!</div>
+      <div>Hello World!</div>
+    </div>
+    <!-- <div class="bg-gradient-to-r from-cyan-500 to-blue-500 ... h-20"></div> -->
+  </main>
+</template>
