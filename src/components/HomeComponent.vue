fd401307cdfa263d11cb77c76a6b1c9a200e8b24
hiram.rubio <floreshiram03@gmail.com>
2022-05-26 15:29:17 -0500

    feature: added home page body content

---
+++ src/components/HomeComponent.vue
@@ -0,0 +1,25 @@
+<script></script>
+<template>
+  <main class="flex justify-center">
+    <div class="w-[60rem] flex flex-row flex-wrap justify-between p-10">
+      <div class="w-[25rem] mt-20">
+        <p class="text-xl pb-5">Hola! soy</p>
+        <p class="text-6xl pb-10">Hiram Rubio</p>
+        <div class="pb-10">
+          <button
+            class="bg-gradient-to-r from-cyan-500 to-blue-500 px-6 py-3 text-white rounded-lg"
+          >
+            See my work
+          </button>
+        </div>
+        <p class="text-6xl pb-10">Hobbies</p>
+        <p class="text-xl pb-5">Escuchar Riddim</p>
+        <p class="text-xl pb-5">Pintar playeras</p>
+        <p class="text-xl pb-5">Salir a conocer nuevos lugares</p>
+      </div>
+      <div class="w-[25rem] text-center mt-20">
+        <img src="../assets/yo.jpg" alt="image" />
+      </div>
+    </div>
+  </main>
+</template>
